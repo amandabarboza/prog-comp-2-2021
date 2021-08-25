d9ef28b62d61f9b75587593acd7ff8ceba668a16
Amanda Barboza <amandab.frc@gmail.com>
2021-08-25 20:06:44 -0300

    25-08

---
+++ sistemavendas.js
@@ -0,0 +1,45 @@
+let sistemaVendas = () =>
+{
+    let opcao
+    let vetVendedores = []
+    do {
+        opcao = prompt(`Informe: \n1. Cadastrar vendedor  \n2. Cadastrar venda \n3. Sair`)
+        switch (opcao)
+        {
+            case 1: let objeto =
+            {
+                codigo: Number(prompt(`Informe o código:`)),
+                nome: prompt(`Informe o nome`),
+                rg: prompt(`Informe o RG`)
+            }
+            // verifica se já tem outro vendedor com o mesmo código
+            let achou = false
+            // vetVendedores.length retorna o tamanho do vetor
+            for (i=0; i<vetVendedores.length; i++)
+            {  
+                if (vetVendedores[i].codigo == objeto.codigo)
+                {
+                    achou = true // achei - não posso cadastrar
+                }
+
+            }
+            if (!achou)
+            {
+                //adiciona objeto no vetor
+                vetVendedores.push(objeto)
+                alert(`Vendedor cadastrado com sucesso`)
+            }
+            else 
+            {
+                alert(`Vendedor já existe com este código`)
+            }
+                break
+            case 2:
+                break
+            case 3: alert(`O programa será encerrado`)
+                break
+            default: alert(`Opção inválida`)            
+        }
+    } 
+    while (opcao != 3)
+}
